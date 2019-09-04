9e97f1ae70bbedbb298de8a4a24e11dea546908e
Lorenzo Ambrosini <lorenzo.ambrosini@mail.polimi.it>
2019-09-04 13:17:26 +0200

    Update gioco1.js
    salto -150

--- gioco1.js
+++ gioco1.js
@@ -387,7 +387,7 @@ function update() {
     if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer)
     {
 
-        player.body.velocity.y = -250;
+        player.body.velocity.y = -400;
         jumpTimer = game.time.now + 750;
 
 
